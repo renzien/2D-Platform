7cd5110f00c57dfa4b79b7af57fb50ae73d9fa46
renzien <alifrizki1011@gmail.com>
2022-10-04 17:49:08 +0700

    update: moving platform

--- 2D Platform Games/Assets/Scripts/StickyPlatform.cs
+++ 2D Platform Games/Assets/Scripts/StickyPlatform.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class StickyPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
     }
 }
